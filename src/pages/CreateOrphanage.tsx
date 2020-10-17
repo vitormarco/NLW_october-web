af889417f02f902cf9bff1f4fc0ff8ef8f17b042
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-17 10:27:55 -0300

    Desativando zoom pelo scroll wheel

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -112,6 +112,7 @@ export default function CreateOrphanage() {
               style={{ width: '100%', height: 280 }}
               zoom={15}
               onclick={handlerMapClick}
+              scrollWheelZoom={false}
             >
               <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
